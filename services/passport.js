ae06f020c1c6ed5b79d0a7f570c6b580fae70bb2
Stephen Kang <stephenkang@Stephens-MacBook-Pro.local>
2018-06-26 11:30:07 -0700

    Add redirect

--- services/passport.js
+++ services/passport.js
@@ -21,6 +21,7 @@ passport.use(new GoogleStrategy(
     {
         clientID: keys.googleClientID,
         clientSecret: keys.googleClientSecret,
+        callbackURL: '/auth/google/callback',
         proxy: true
     },
     (accessToken, refreshToken, profile, done) => {
