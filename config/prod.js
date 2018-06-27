8f48d39041198ea5d38bd7991487df5ca8656ed4
Stephen Kang <stephenkang@Stephens-MacBook-Pro.local>
2018-06-26 18:36:01 -0700

    Change typo

--- config/prod.js
+++ config/prod.js
@@ -2,7 +2,7 @@ module.exports = {
     googleClientID: process.env.GOOGLE_CLIENT_ID,
     googleClientSecret: process.env.GOOGLE_CLIENT_SECRET,
     mongoURI: process.env.MONGO_URI,
-    cookieKey: process.env.COOKIE_KEY
+    cookieKey: process.env.COOKIE_KEY,
     facebookClientID: process.env.FACEBOOK_CLIENT_ID,
     facebookClientSecret: process.env.FACEBOOK_CLIENT_SECRET,
 };
