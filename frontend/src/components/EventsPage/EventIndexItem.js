6a27c4e17048ecb22b5fa594bb3765fa0578e563
Ryan Alejandro <alejandr257@gmail.com>
2018-06-29 11:57:45 -0700

    Update EventIndexItem to show db information

--- frontend/src/components/EventsPage/EventIndexItem.js
+++ frontend/src/components/EventsPage/EventIndexItem.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import { View, Image, Text, StyleSheet } from 'react-native';
 
 export default class EventIndexItem extends Component {
   constructor(props) {
@@ -8,16 +8,22 @@ export default class EventIndexItem extends Component {
 
   render() {
     console.log(this.props);
+    const { name, host, description, address, start_time, end_time, date, cost, image } = this.props.item;
     return ( <View style={[style.container]}>
         <View style={{flex: 2}}>
-          <Text>Event Name:</Text>
+          <Text>Event Name:{ name }</Text>
+          <Image source={{ uri:image }} style={{height: 25, width: 25}}></Image>
         </View>
         <View style={{flex: 1}}>
-          <Text>Event Date</Text>
-          <Text>Event Location</Text>
+          <Text>Event Host: { host }</Text>
+          <Text>Event Date: { date }</Text>
+          <Text>Event Start Time: { start_time }</Text>
+          <Text>Event End Time: { end_time }</Text>
+          <Text>Event Location: { address }</Text>
+          <Text>Event Cost: { cost }</Text>
         </View>
         <View style={{flex: 1}}>
-          <Text>Event Description</Text>
+          <Text>Event Description: { description } </Text>
         </View>
       </View>);
   }
